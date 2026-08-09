cbee6b073b2b42ba3a1b3522db9b5bc8d606c736
spappas9000 <sppappas8@gmail.com>
2026-08-09 17:17:52 -0500

    Adding models and EDA work

--- eda_work.R
+++ eda_work.R
@@ -1,4 +1,8 @@
 
+install_github("BillPetti/baseballr")
+
+install.packages("baseballr")
+
 library(tidyverse)
 library(baseballr)
 library(data.table)
@@ -15,7 +19,7 @@ statcast_bind_rows <- function(start_date, end_date, player_type) {
   }
   
   stats_list <- map2_df(range, range_offset, function(x, y) {
-    statcast_search(
+    baseballr::statcast_search(
       start_date = x,
       end_date = y,
       player_type = player_type
@@ -33,10 +37,28 @@ statcastlist_26 = list(
       month = 4,
       across(where(is.character), ~ na_if(., ""))
     ),
-  may26 = statcast_bind_rows(start_date = "2026-05-01", end_date = "2026-05-14", player_type = "pitcher") %>%
+  may26 = statcast_bind_rows(start_date = "2026-05-01", end_date = "2026-05-31", player_type = "pitcher") %>%
     distinct() %>%
     mutate(
-      month = 4,
+      month = 5,
+      across(where(is.character), ~ na_if(., ""))
+    ),
+  june26 = statcast_bind_rows(start_date = "2026-06-01", end_date = "2026-06-30", player_type = "pitcher") %>%
+    distinct() %>%
+    mutate(
+      month = 6,
+      across(where(is.character), ~ na_if(., ""))
+    ),
+  july26_1 = statcast_bind_rows(start_date = "2026-07-01", end_date = "2026-07-12", player_type = "pitcher") %>%
+    distinct() %>%
+    mutate(
+      month = 7,
+      across(where(is.character), ~ na_if(., ""))
+    ),
+  july26_2 = statcast_bind_rows(start_date = "2026-07-16", end_date = "2026-07-31", player_type = "pitcher") %>%
+    distinct() %>%
+    mutate(
+      month = 7,
       across(where(is.character), ~ na_if(., ""))
     )
 ) %>% 
@@ -45,18 +67,11 @@ statcastlist_26 = list(
 challenge = list(
   catcher = read_csv("data/catcher.csv") %>%
     mutate(challenger = "Catcher"),
-  pitcher = read_csv("data/pitcher.csv") %>%
-    mutate(challenger = "Pitcher"),
   hitter = read_csv("data/hitter.csv") %>%
     mutate(challenger = "Hitter")
 ) %>%
   rbindlist() %>%
   mutate(
-    call_change = case_when(
-      challenger == "Catcher" ~ ifelse(description == "called_strike", 1, 0),
-      challenger == "Pitcher" ~ ifelse(description == "called_strike", 1, 0),
-      challenger == "Hitter" ~ ifelse(description == "ball", 1, 0)
-    ),
     pitch_class = case_when(
       pitch_type %in% c("FF", "SI", "FC") ~ "Fastball",
       pitch_type %in% c("CH", "FS", "FO", "SC") ~ "Offspeed",
@@ -68,7 +83,9 @@ challenge = list(
       .default = inning
     ),
     challenge_hitter = ifelse(challenger == "Hitter", 1, 0),
-    challenge_catcher = ifelse(challenger == "Catcher", 1, 0)
+    challenge_catcher = ifelse(challenger == "Catcher", 1, 0),
+    call_change_hitter = ifelse(challenger == "Hitter" & description == "ball", 1, 0),
+    call_change_catcher = ifelse(challenger == "Catcher" & description == "called_strike", 1, 0)
   )
 
 table(challenge$challenger)/nrow(challenge)
@@ -139,75 +156,42 @@ chase <- read_csv("data/chase.csv") %>%
   mutate(batter = as.character(player_id)) %>%
   select(batter, 3:5)
 
-## ---- Modeling ----
+modeldata %>%
+  filter(outs_when_up == 1, !is.na(on_1b), !is.na(on_2b), !is.na(on_3b), home_team == bat_team, description == "ball", 
+         count == "0-0", home_score_diff == 2) %>%
+  arrange(-desc(inning)) %>%
+  select(outs_when_up, on_1b, on_2b, on_3b, home_team, bat_team, description, count, inning, home_score_diff, delta_home_win_exp, delta_run_exp) %>%
+  view()
 
-library(lme4)
+modeldata %>%
+  filter(!is.na(on_1b), !is.na(on_2b), !is.na(on_3b), count == "3-2", description != "called_strike") %>%
+  view()
 
-modeldata <- rbind(challenge, statcastlist_26, fill = T) %>%
-  filter(!description %in% c("swinging_strike", "hit_into_play", "foul_tip", "foul", "foul_bunt", 
-                             "missed_bunt")) %>%
-  distinct(game_pk, at_bat_number, pitch_number, .keep_all = T) %>%
-  mutate(
-    count = paste0(balls, "-", strikes),
-    challenge_hitter = ifelse(is.na(challenge_hitter), 0, challenge_hitter),
-    challenge_catcher = ifelse(is.na(challenge_catcher), 0, challenge_catcher),
-    call_change = ifelse(is.na(call_change), 0, call_change),
-    plate_z_adj = plate_z - ((sz_top - sz_bot)/2 + sz_bot),
-    plate_x_adj = case_when(stand == "L" & plate_x >= 0 ~ -abs(plate_x),
-                            stand == "L" & plate_x < 0 ~ abs(plate_x),
-                            stand == "R" & plate_x >= 0 ~ abs(plate_x),
-                            stand == "R" & plate_x < 0 ~ -abs(plate_x)),
-    bat_team = ifelse(inning_topbot == "Top", away_team, home_team),
-    def_team = ifelse(inning_topbot == "Top", home_team, away_team),
-  )
+winprobabilities <- modeldata %>%
+  mutate(on_1b_ind = ifelse(!is.na(on_1b), 1, 0), on_2b_ind = ifelse(!is.na(on_2b), 1, 0), on_3b_ind = ifelse(!is.na(on_3b), 1, 0),
+         delta_runexp = ifelse(home_team == bat_team, delta_run_exp, delta_run_exp * -1), 
+         scorediff = ifelse(home_team == bat_team, bat_score_diff, bat_score_diff * -1),
+         description_ind = ifelse(description == "called_strike", "strike", "ball")) %>%
+  group_by(on_1b_ind, on_2b_ind, on_3b_ind, outs_when_up, balls, strikes, description_ind) %>%
+  reframe(delta_run_exp_2 = mean(delta_run_exp), max_runexp = max(delta_run_exp), min_runexp = min(delta_run_exp), 
+          N = n()) %>%
+  mutate(rowid = row_number()) %>%
+  view()
 
-mod0_call_change <- glm(
-  is_challenge ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj,
-  data = modeldata,
-  family = "binomial"
-)
+winprobabilities2 <- merge(winprobabilities, winprobabilities, by = c(1:6)) %>%
+  filter(rowid.x != rowid.y) %>%
+  rename(description_ind = description_ind.x) %>%
+  view()
 
-mod1_call_change_hitter <- glmer(
-  challenge_hitter ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj + (1|batter) +
-                     (1|bat_team) + inning + bat_score_diff + count,
-  data = modeldata,
-  family = "binomial",
-  control = glmerControl(optimizer = "bobyqa")
-)
+length(which(winprobabilities2$delta == 0))/nrow(winprobabilities2)
 
-mod1_call_change_catcher <- glmer(
-  challenge_catcher ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj + (1|fielder_2) + 
-    (1|def_team),
-  data = modeldata,
-  family = "binomial",
-  control = glmerControl(optimizer = "bobyqa")
-)
+modeldata %>%
+  mutate(on_1b_ind = ifelse(!is.na(on_1b), 1, 0), on_2b_ind = ifelse(!is.na(on_2b), 1, 0), on_3b_ind = ifelse(!is.na(on_3b), 1, 0)) %>%
+  filter(on_1b_ind == 1, on_2b_ind == 1, on_3b_ind == 0, outs_when_up == 0, balls == 3, strikes == 2,
+         description == "called_strike") %>%
+  view()
 
-re <- ranef(mod1_call_change_hitter, condVar = TRUE)
-
-hitter_re <- re$batter
-
-post_var <- attr(hitter_re, "postVar")
-
-re_exploration <- data.frame(
-  batter = rownames(hitter_re),
-  estimate = hitter_re[, "(Intercept)"],
-  se = sqrt(post_var[1, 1, ])
-) %>%
-  mutate(
-    lower = estimate - 1.96 * se,
-    upper = estimate + 1.96 * se
-  )
-  left_join(chase, by = "batter")
-
-anova(mod1_call_change, mod0_call_change)
-
-summary(mod0_call_change)
-
-summary(mod1_call_change_hitter)
-
-re_exploration %>%
-  filter(total_pitches >= 100) %>%
-  ggplot(aes(x = pitch_percent, y = lower)) +
-  geom_point() +
-  geom_smooth(method = "lm")
+modeldata %>%
+  select(on_1b_ind, on_2b_ind, on_3b_ind, outs_when_up, balls, strikes, description_ind, description_ind.y.y, delta) %>%
+  view()
+  
---
+++ model_work.R
@@ -0,0 +1,182 @@
+
+library(lme4)
+library(glmmTMB)
+library(tidyverse)
+library(baseballr)
+library(httr)
+library(jsonlite)
+library(tidyverse)
+library(httr2)
+
+modeldata <- rbind(challenge, statcastlist_26, fill = T) %>%
+  filter(!description %in% c("swinging_strike", "hit_into_play", "foul_tip", "foul", "foul_bunt", "missed_bunt", "swinging_strike_blocked",
+                             "bunt_foul_tip", "automatic_ball", "automatic_strike", "hit_by_pitch", "swinging_pitchout"),
+         !des %in% c("Cedric Mullins called out on strikes. Cedric Mullins to 1st. Passed ball by catcher Mickey Gasper.",
+                    "Jacob Young called out on strikes. Jacob Young to 1st. Passed ball by catcher Gabriel Moreno."),
+         grepl("catcher interference", des) == F) %>%
+  distinct(game_pk, at_bat_number, pitch_number, .keep_all = T) %>%
+  mutate(
+    count = paste0(balls, "-", strikes),
+    challenge_hitter = ifelse(is.na(challenge_hitter), 0, challenge_hitter),
+    challenge_catcher = ifelse(is.na(challenge_catcher), 0, challenge_catcher),
+    call_change_hitter = ifelse(is.na(call_change_hitter), 0, call_change_hitter),
+    call_change_catcher = ifelse(is.na(call_change_catcher), 0, call_change_catcher),
+    plate_z_adj = plate_z - ((sz_top - sz_bot)/2 + sz_bot),
+    plate_x_adj = case_when(stand == "L" & plate_x >= 0 ~ -abs(plate_x),
+                            stand == "L" & plate_x < 0 ~ abs(plate_x),
+                            stand == "R" & plate_x >= 0 ~ abs(plate_x),
+                            stand == "R" & plate_x < 0 ~ -abs(plate_x)),
+    bat_team = ifelse(inning_topbot == "Top", away_team, home_team),
+    def_team = ifelse(inning_topbot == "Top", home_team, away_team),
+    def_score_diff = ifelse(away_team == def_team, bat_score_diff * -1, bat_score_diff),
+    on_1b_ind = ifelse(!is.na(on_1b), 1, 0),
+    on_2b_ind = ifelse(!is.na(on_2b), 1, 0),
+    on_3b_ind = ifelse(!is.na(on_1b), 1, 0),
+    description_ind = ifelse(description == "called_strike", "strike", "ball")
+  ) %>%
+  left_join(winprobabilities2, by = c("on_1b_ind", "on_2b_ind", "on_3b_ind", "balls", "strikes", "outs_when_up", "description_ind")) %>%
+  mutate(delta = abs(delta_run_exp_2.x - delta_run_exp_2.y))
+
+game_pks <- unique(modeldata$game_pk)
+
+df <- data.frame()
+
+for (game_id in game_pks){
+  url <- paste0('http://statsapi.mlb.com/api/v1.1/game/', game_id, '/feed/live')
+  response <- GET(url)
+  content <- content(response, as = "text")
+  json_data <- fromJSON(content)
+  
+  umps <- json_data$liveData$boxscore$officials$official
+  roles <- json_data$liveData$boxscore$officials$officialType
+  
+  umps <- cbind(umps, roles) %>%
+    filter(roles == "Home Plate") %>%
+    mutate(game_pk = game_id) %>%
+    select(game_pk, fullName)
+  
+  df <- rbind(df, umps)
+}
+
+modeldata_hitter <- modeldata %>%
+  filter(description == "called_strike") %>%
+  mutate(challenge_hitter_lost = challenge_hitter == 1 & call_change_hitter == 0) %>%
+  arrange(game_pk, bat_team, -desc(at_bat_number), -desc(pitch_number)) %>%
+  group_by(game_pk, bat_team) %>%
+  mutate(
+    challenges_remaining = pmax(2 - lag(cumsum(challenge_hitter_lost), default = 0), 0)
+  ) %>%
+  ungroup() %>%
+  filter(challenges_remaining != 0) %>%
+  left_join(df %>% rename(umpire_hp = fullName), by = "game_pk")
+
+modeldata_catcher <- modeldata %>%
+  filter(description != "called_strike") %>%
+  mutate(challenge_catcher_lost = challenge_catcher == 1 & call_change_catcher == 0) %>%
+  arrange(game_pk, def_team, -desc(at_bat_number), -desc(pitch_number)) %>%
+  group_by(game_pk, def_team) %>%
+  mutate(
+    challenges_remaining = pmax(2 - lag(cumsum(challenge_catcher_lost), default = 0), 0)
+  ) %>%
+  ungroup() %>%
+  filter(challenges_remaining != 0) %>%
+  left_join(df %>% rename(umpire_hp = fullName), by = "game_pk")
+
+## ---- Models ----
+
+mod0_call_change_hitter <- glm(
+  challenge_hitter ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj,
+  data = modeldata_hitter,
+  family = "binomial"
+)
+
+summary(mod0_call_change_hitter)
+
+mod1_call_change_hitter <- glm(
+  challenge_hitter ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj + delta + inning + bat_score_diff + challenges_remaining,
+  data = modeldata_hitter,
+  family = "binomial"
+)
+
+summary(mod1_call_change_hitter)
+
+mod2_call_change_hitter <- glmer(
+  challenge_hitter ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj + delta + inning + bat_score_diff + challenges_remaining +
+                     (1|batter) + (1|bat_team) + (1|umpire_hp),
+  data = modeldata_hitter,
+  family = "binomial",
+  control = glmerControl(optimizer = "bobyqa")
+)
+
+summary(mod2_call_change_hitter)
+
+AIC(mod1_call_change_hitter, mod2_call_change_hitter)
+
+null.id <- -2 * logLik(mod1_call_change_hitter) + 2 * logLik(mod2_call_change_hitter)
+
+pchisq(as.numeric(null.id), df = 1, lower.tail = FALSE)
+
+mod0_call_change_catcher <- glm(
+  challenge_catcher ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj,
+  data = modeldata_catcher,
+  family = "binomial"
+)
+
+summary(mod0_call_change_catcher)
+
+mod1_call_change_catcher <- glm(
+  challenge_catcher ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj + delta + inning + bat_score_diff + challenges_remaining,
+  data = modeldata_catcher,
+  family = "binomial"
+)
+
+summary(mod1_call_change_catcher)
+
+p <- fitted(mod1_call_change_catcher)
+
+summary(p)
+
+challengeprob <- predict(mod1_call_change_catcher, newdata = modeldata_catcher, type = "response")
+
+cbind(modeldata_catcher, challengeprob) %>%
+  view()
+
+modeldata_catcher %>%
+  mutate(
+    challengeprob = predict(mod1_call_change_catcher, type = "response")
+  ) %>%
+  view()
+
+mod2_call_change_catcher <- glmer(
+  challenge_catcher ~ I(plate_z_adj^2) + I(plate_x_adj^2) + plate_z_adj + plate_x_adj + delta + inning + bat_score_diff + challenges_remaining +
+                      (1|fielder_2) + (1|def_team) + (1|umpire_hp),
+  data = modeldata_catcher,
+  family = "binomial",
+  control = glmerControl(optimizer = "bobyqa")
+)
+
+summary(mod2_call_change_catcher)
+
+re <- ranef(mod2_call_change_catcher, condVar = TRUE)
+
+hitter_re <- re$fielder_2
+
+post_var <- attr(hitter_re, "postVar")
+
+re_exploration <- data.frame(
+  batter = rownames(hitter_re),
+  estimate = hitter_re[, "(Intercept)"],
+  se = sqrt(post_var[1, 1, ])
+) %>%
+  mutate(
+    lower = estimate - 1.96 * se,
+    upper = estimate + 1.96 * se
+  )
+
+re_exploration %>%
+  filter(total_pitches >= 100) %>%
+  ggplot(aes(x = pitch_percent, y = lower)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
